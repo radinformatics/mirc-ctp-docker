b157abb428ac4947d424bd45215c72b37b9a1ce2
Maxwell Cheong <maxicheong@gmail.com>
2017-05-08 22:13:11 -0700

    During start, put in default anonymization pipeline by putting in the pre-configured config.xml

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 mv /code/CTP/* /JavaPrograms
+
+# put in default pipeline configuration of CTP
+mv /code/config.xml /JavaPrograms
+
 mv /code/ctpService-ubuntu.sh /JavaPrograms/linux/ctpService-ubuntu.sh
 ln -s /JavaPrograms/linux/ctpService-ubuntu.sh /etc/init.d/ctpService
 update-rc.d ctpService defaults 98 02
